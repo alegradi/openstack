b5e9f9cc227ede9caebf0550701b7af51b87b966
Adam Legradi <adam.legradi@protonmail.ch>
2019-11-27 18:18:58 +0100

    Renamed node1.yml to allnodes.yml, updated inv with a second node and specified StrictHostKeyChecking=no, instructions updated, terraform now contains 2 nodes

--- hetzner_multi_testing/terraform.tf
+++ hetzner_multi_testing/terraform.tf
@@ -7,7 +7,22 @@ resource "hcloud_server" "node1" {
   image         = "centos-7"
   server_type   = "cx11"
   ssh_keys      = ["${data.hcloud_ssh_key.centos7-terraform_key.id}"]
+
+# Calling Ansible playbook from Terraform currently commented out and the playbook is called manually 
+#  provisioner "local-exec" {
+#   command	= "ansible-playbook -i '${hcloud_server.node1.ipv4_address},' --key-file=~/.ssh/terraform_pwless node1.yml -e 'ANSIBLE_HOST_KEY_CHECKING=False'" 
+#  }
 }
+
+# Node-2 configuration
+resource "hcloud_server" "node2" {
+  name          = "node2"
+  image         = "centos-7"
+  server_type   = "cx11"
+  ssh_keys      = ["${data.hcloud_ssh_key.centos7-terraform_key.id}"]
+}
+
+
 
 # Private netwok configuration
 resource "hcloud_network" "openstack_network" {
@@ -25,13 +40,21 @@ resource "hcloud_network_subnet" "openstack_vlan" {
 resource "hcloud_server_network" "srvnetwork1" {
   server_id     = hcloud_server.node1.id
   network_id    = hcloud_network.openstack_network.id
-  ip            = "10.0.2.4"
+  ip            = "10.0.2.1"
 }
 
+resource "hcloud_server_network" "srvnetwork2" {
+  server_id     = hcloud_server.node2.id
+  network_id    = hcloud_network.openstack_network.id
+  ip            = "10.0.2.2"
+}
 
 output "public_ip4_node1" {
   value         = "${hcloud_server.node1.ipv4_address}"
 }
 
+output "public_ip4_node2" {
+  value         = "${hcloud_server.node2.ipv4_address}"
+}
 
 
