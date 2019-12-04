4028fffa4399de856ddf6822f21391096b5d8404
Adam Legradi <adam.legradi@protonmail.ch>
2019-12-04 19:12:25 +0100

    updated terraform to create two private networks, written a description of the network into the instructions.txt

--- hetzner_multi_testing/terraform.tf
+++ hetzner_multi_testing/terraform.tf
@@ -23,15 +23,14 @@ resource "hcloud_server" "node2" {
 }
 
 
-
-# Private netwok configuration
-resource "hcloud_network" "openstack_network" {
-  name          = "openstack-network"
+# Private netwok configuration - Management network
+resource "hcloud_network" "openstack_man_network" {
+  name          = "openstack-man-network"
   ip_range      = "10.0.0.0/8"
 }
 
-resource "hcloud_network_subnet" "openstack_vlan" {
-  network_id    = hcloud_network.openstack_network.id
+resource "hcloud_network_subnet" "openstack_man_vlan1" {
+  network_id    = hcloud_network.openstack_man_network.id
   network_zone  = "eu-central"
   type          = "server"
   ip_range      = "10.0.2.0/24"
@@ -39,15 +38,44 @@ resource "hcloud_network_subnet" "openstack_vlan" {
 
 resource "hcloud_server_network" "srvnetwork1" {
   server_id     = hcloud_server.node1.id
-  network_id    = hcloud_network.openstack_network.id
+  network_id    = hcloud_network.openstack_man_network.id
   ip            = "10.0.2.1"
 }
 
 resource "hcloud_server_network" "srvnetwork2" {
   server_id     = hcloud_server.node2.id
-  network_id    = hcloud_network.openstack_network.id
+  network_id    = hcloud_network.openstack_man_network.id
   ip            = "10.0.2.2"
 }
+
+
+# Private netwok configuration - Tenant network
+resource "hcloud_network" "openstack_ten_network" {
+  name          = "openstack-ten-network"
+  ip_range      = "192.168.0.0/16"
+}
+
+resource "hcloud_network_subnet" "openstack_ten_vlan1" {
+  network_id    = hcloud_network.openstack_ten_network.id
+  network_zone  = "eu-central"
+  type          = "server"
+  ip_range      = "192.168.10.0/24"
+}
+
+resource "hcloud_server_network" "srvnetwork3" {
+  server_id     = hcloud_server.node1.id
+  network_id    = hcloud_network.openstack_ten_network.id
+  ip            = "192.168.10.1"
+}
+
+resource "hcloud_server_network" "srvnetwork4" {
+  server_id     = hcloud_server.node2.id
+  network_id    = hcloud_network.openstack_ten_network.id
+  ip            = "192.168.10.2"
+}
+
+
+
 
 output "public_ip4_node1" {
   value         = "${hcloud_server.node1.ipv4_address}"
